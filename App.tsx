4d3d8e624efb3d87af8823a027485423ef292572
Niklas Reinhold <niklas.reinhold2000@gmail.com>
2023-07-30 20:21:19 -0500

    add react native svg transformer and show svg

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Globus from "./assets/globus.svg";
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Globus width={120} height={120}></Globus>
       <StatusBar style="auto" />
     </View>
   );
