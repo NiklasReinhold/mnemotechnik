36e185811c394df0ec52828af3e689a38582564c
Niklas Reinhold <niklas.reinhold2000@gmail.com>
2023-07-30 23:51:40 -0500

    add link button and change text font

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Dimensions, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import globus from "./assets/globus.svg";
+import * as Linking from 'expo-linking';
 
 export default function App() {
   var dimensions = Dimensions.get('window');
@@ -18,11 +19,12 @@ export default function App() {
     ["Feuerland", "Falkland-Inseln", "St. Helena", "Urwald", "Savanne", "Seychellen", "Neu-Guinea", "Osterinsel"],
     ["Weltall", "Sonne", "Eismeer", "Antarktis", "Capetown", "Madagaskar", "Australien", "Neuseeland"]
   ]
-  //http://www.p-roesler.de/memory.html#Globus-Merksystem
+  const link = 'http://www.p-roesler.de/memory.html#Globus-Merksystem';
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={{position: 'absolute', left: 8, top: 8, backgroundColor: '#990000b0', borderRadius: 8}}>
+      <TouchableOpacity style={{position: 'absolute', left: 8, top: 8, backgroundColor: '#990000b0', borderRadius: 8}}
+        onPress={() => Linking.openURL(link)}>
         <Text style={{padding: 8}}>Globus-Merksystem</Text>
       </TouchableOpacity>
       <View style={{width: min*1.01, height: min, left: -12}}> 
@@ -33,7 +35,11 @@ export default function App() {
         data={countries.flat()}
         renderItem={({item}) => (
           <View style={{width: elementSize, height: elementSize, justifyContent: 'center', borderWidth: 1}}>
-            <Text style={{textAlign: 'center'}}>{item}</Text>
+            <Text style={{textAlign: 'center', color: '#990000ff', fontWeight: 'bold', 
+              textShadowColor: 'white',
+              fontSize: 12,
+              textShadowOffset: {width: -1, height: 1},
+              textShadowRadius: 0}}>{item}</Text>
           </View>
         )}
         keyExtractor={(item, index) => index.toString()}
