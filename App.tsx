ee9b311b7c95f5a8ac6991aaec554cf1c5b47d07
Niklas Reinhold <niklas.reinhold2000@gmail.com>
2023-07-30 20:21:32 -0500

    show svg, add countries grid, add link

--- App.tsx
+++ App.tsx
@@ -1,15 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import Globus from "./assets/globus.svg";
+import globus from "./assets/globus.svg";
 
 export default function App() {
+  var dimensions = Dimensions.get('window');
+  var min = Math.min(dimensions.width, dimensions.height);
+
+  var countries =  [
+    ["Alaska", "Nordpol", "Island", "Norwegen", "Schweden", "Finland", "Dresden", "Regensburg"],
+    ["Kanada", "Grönland", "England", "West-Deutschland", "Ost-Deutschland", "Polen", "Chemnitz", "Bayern"],
+    ["Wilder Westen", "USA-Oststaaten", "Frankreich", "Schweiz", "Österreich", "Rumänien", "Russland", "Sibirien"],
+    ["Panama", "Kuba", "Spanien", "Italien", "Griechenland", "Türkei", "China", "Japan"],
+    ["Galapagos Inseln", "Brasilien", "Atlantik", "Mittelmeer", "Israel", "Iran", "Indien", "Indochina"],
+    ["Chile", "Argentinien", "Kanarische Inseln", "Sahara", "Ägypten", "Saudi-Arabien", "Indonesien", "Hawaii"],
+    ["Feuerland", "Falkland-Inseln", "St. Helena", "Urwald", "Savanne", "Seychellen", "Neu-Guinea", "Osterinsel"],
+    ["Weltall", "Sonne", "Eismeer", "Antarktis", "Südafrika", "Madagaskar", "Australien", "Neuseeland"]
+  ]
+  //http://www.p-roesler.de/memory.html#Globus-Merksystem
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Globus width={120} height={120}></Globus>
-      <StatusBar style="auto" />
+      <TouchableOpacity style={{position: 'absolute', left: 8, top: 8, backgroundColor: '#990000b0', borderRadius: 8}}>
+        <Text style={{padding: 8}}>Globus-Merksystem</Text>
+      </TouchableOpacity>
+      <View style={{width: min, height: min, }}> 
+        <View style={{backgroundColor: '#00ff00b0'}}>
+          <object type="image/svg+xml" aria-label="globus" data={globus.toString()}></object>
+        </View>
+      </View>
+      <FlatList
+        style={{position: 'absolute', width: min, height: min,}}
+        data={countries.flat()}
+        renderItem={({item}) => (
+          <View style={{width: min/8, height: min/8, justifyContent: 'center', borderWidth: 1}}>
+            <Text style={{textAlign: 'center'}}>{item}</Text>
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={8}
+      />
     </View>
   );
 }
